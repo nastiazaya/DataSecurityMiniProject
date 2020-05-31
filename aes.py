8eaf17c84bedabc1da6a7782e711c36ee70a3068
nastiazaya <nastiazaytzev@gmail.com>
2020-06-01 00:21:27 +0300

    Add AES algo

---
+++ aes.py
@@ -0,0 +1,68 @@
+import ast
+from base64 import b64encode, b64decode
+import hashlib
+from Cryptodome.Cipher import AES
+import os
+from Cryptodome.Random import get_random_bytes
+
+
+def encrypt(plain_text, password):
+    # generate a random salt
+    salt = get_random_bytes(AES.block_size)
+
+    # use the Scrypt KDF to get a private key from the password
+    private_key = hashlib.scrypt(
+        password.encode(), salt=salt, n=2 ** 14, r=8, p=1, dklen=32)
+
+    # create cipher config
+    cipher_config = AES.new(private_key, AES.MODE_GCM)
+
+    # return a dictionary with the encrypted text
+    cipher_text, tag = cipher_config.encrypt_and_digest(bytes(plain_text, 'utf-8'))
+    return str({
+        'cipher_text': b64encode(cipher_text).decode('utf-8'),
+        'salt': b64encode(salt).decode('utf-8'),
+        'nonce': b64encode(cipher_config.nonce).decode('utf-8'),
+        'tag': b64encode(tag).decode('utf-8')
+    })
+
+
+def decrypt(enc_dict, password):
+    res = ast.literal_eval(enc_dict)
+    # decode the dictionary entries from base64
+    salt = b64decode(res['salt'])
+    cipher_text = b64decode(res['cipher_text'])
+    nonce = b64decode(res['nonce'])
+    tag = b64decode(res['tag'])
+
+    # generate the private key from the password and salt
+    private_key = hashlib.scrypt(
+        password.encode(), salt=salt, n=2 ** 14, r=8, p=1, dklen=32)
+
+    # create the cipher config
+    cipher = AES.new(private_key, AES.MODE_GCM, nonce=nonce)
+
+    # decrypt the cipher text
+    try:
+        decrypted = cipher.decrypt_and_verify(cipher_text, tag)
+        return decrypted
+    except ValueError:
+        raise
+
+
+def main():
+    password = input("Password: ")
+
+    # First let us encrypt secret message
+    encrypted = encrypt("The secretest message here", password)
+    print(encrypted)
+
+    # Let us decrypt using our original password
+    try:
+        decrypted = decrypt(encrypted,password)
+        print(bytes.decode(decrypted))
+    except ValueError:
+        print('error')
+
+
+main()
